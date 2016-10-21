c6c52afb478e5928249c87e59c570606daf3608e
bahelms <jimbonkgit@gmail.com>
2016-10-21 06:21:53 -0400

    adds main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.info("Parser started.")
+
+# get list of emails
+# for email in emails
+#     fetch email
+#     decode
+#     parse articles
+#     save articles
+#     archive email
