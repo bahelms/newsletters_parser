ba3221312fcd4dc4e7968830e89cd84e3d106beb
bahelms <jimbonkgit@gmail.com>
2016-10-29 21:46:02 -0400

    begins implementing html parsing

--- parser/parser.py
+++ parser/parser.py
@@ -1,10 +1,19 @@
+import logging
+from lxml import etree
+from typing import Tuple
+
+logger = logging.getLogger(__name__)
+
 class Parser(object):
-    """Parses HTML strings into Article models"""
+    """Extracts data for articles from HTML strings"""
 
-    def __init__(self, message):
-        """
-        :message: HTML string
-        :returns: Article model
+    def __init__(self, html: str):
+        """Takes a string of HTML"""
+        self.html = html
 
+    def articles(self):
+        """Returns a list of dictionaries containing article data
+
+        :returns: List[Dict]
         """
-        self.message = message
+        return [{}]
--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -1,8 +1,13 @@
 import unittest
+from parser import Parser
 
 class TestParser(unittest.TestCase):
-    def test_something(self, arg1):
-        pass
+    def setUp(self):
+        with open("tests/support/html_message.html") as f:
+            self.parser = Parser(f.read())
+
+    def test_article_data_is_parsed_from_html(self):
+        self.assertEqual(len(self.parser.articles()), 24)
 
 if __name__ == "__main__":
     unittest.main()
