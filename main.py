919928994b53ab379363d250e95e5beb286f8ed4
bahelms <jimbonkgit@gmail.com>
2016-11-22 05:46:45 -0500

    implements pending article persistence

--- main.py
+++ main.py
@@ -13,20 +13,18 @@ logger.info("Parser started.")
 user_id = os.environ["GMAIL_USER_ID"]
 client = gmail.Client(user_id)
 msg_ids = client.message_ids()
-
 db = Database(os.environ["PARSER_ENV"])
+
 db.initialize()
 
-# for id in msg_ids:
-#     message = client.get_message(id)
-#     strategy_name = message.newsletter_name().replace(" ", "")
+for id in msg_ids:
+    message = client.get_message(id)
+    strategy_name = message.newsletter_name().replace(" ", "")
 
-#     if strategy_name in dir():
-#         strategy = getattr(sys.modules[__name__], strategy_name)
-#         for article in Parser(message.html(), strategy).articles():
-#             pending_article = PendingArticle(**article)
-#             logger.info(pending_article.title)
-#             # save articles
-#         # archive email
-#     else:
-#         logger.info("No strategy found for {}.".format(strategy_name))
+    if strategy_name in dir():
+        strategy = getattr(sys.modules[__name__], strategy_name)
+        for article in Parser(message.html(), strategy).articles():
+            db.persist_model(PendingArticle(**article))
+        # archive email
+    else:
+        logger.info("No strategy found for {}.".format(strategy_name))
--- parser/database.py
+++ parser/database.py
@@ -1,6 +1,7 @@
 import logging
 import yaml
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,6 +14,7 @@ class Database(object):
             self.config = yaml.load(f.read())["database"][env]
         db_url = self._db_url_template().format(**self.config)
         self.engine = create_engine(db_url)
+        self.Session = sessionmaker(bind=self.engine)
 
     def initialize(self):
         """Sets up database if needed.
@@ -25,10 +27,31 @@ class Database(object):
             conn = self.engine.connect()
             logger.info("DB already exists.")
         except Exception as e:
-            # Create DB
             logger.info("DB not found. Creating...")
+            self._create_app_database()
         else:
             conn.close()
+        logger.info("Creating tables...")
+        self._create_tables()
+
+    def persist_model(self, model):
+        session = self.Session()
+        session.add(model)
+        session.commit()
+
+    def _create_tables(self):
+        from parser.pending_article import Base, PendingArticle
+        Base.metadata.create_all(self.engine)
 
     def _db_url_template(self):
         return "postgres://{username}:{password}@{host}:{port}/{database}"
+
+    def _create_app_database(self):
+        app_db = self.config["database"]
+        self.config["database"] = "postgres"
+        db_url = self._db_url_template().format(**self.config)
+        postgres_engine = create_engine(db_url, echo=True)
+        pg_conn = postgres_engine.connect()
+        pg_conn.execute("commit")
+        pg_conn.execute("create database {0}".format(app_db))
+        pg_conn.close()
